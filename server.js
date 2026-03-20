d01d789932e3d50baa7cea7ff7252aa4adedbc79
Prince Mishra <princemishra@email.com>
2026-03-20 09:53:02 +0530

    update multiply API

--- server.js
+++ server.js
@@ -7,47 +7,14 @@ app.get("/", (req, res) => {
 });
 
 
-app.get("/sum", (req, res) => {
+app.get("/multiplication", (req, res) => {
     const a = parseInt(req.query.a);
     const b = parseInt(req.query.b);
 
-    const result = a - b;
-    res.send("Sum is: " + result);
+    const result = a * b;
+    res.send("multiplication is: " + result);
 });
 
 app.listen(5000, () => {
     console.log("Server running on http://localhost:5000");
 });
-
-// const express = require("express");
-// const app = express();
-
-// app.use(express.json());
-
-// const userRoutes = require("./routes/userRoutes");
-// app.use("/api/users", userRoutes);
-
-// app.listen(5000, () => {
-//     console.log("Server running on http://localhost:5000");
-// });
-
-// const express = require("express");
-// const app = express();
-
-// app.use(express.json());
-
-
-// const userRoutes = require("./routes/userRoutes");
-
-
-// app.use("/api/users", userRoutes);
-
-
-// app.get("/", (req, res) => {
-//     res.send("API is working 🚀");
-// });
-
-
-// app.listen(5000, () => {
-//     console.log("Server running on http://localhost:5000");
-// });
